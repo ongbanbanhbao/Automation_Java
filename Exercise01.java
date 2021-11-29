8f82ee014481129e279c0bfdcfba826f194132a8
Phuong Vo <71152473+ongbanbanhbao@users.noreply.github.com>
2021-11-29 18:09:40 +0700

    Update Exercise01.java

--- Exercise01.java
+++ Exercise01.java
@@ -5,17 +5,13 @@ public class Exercise01 {
 
         String stringGiven = "2hrs and 5 minutes";
 
-
         String hourChaging = stringGiven.substring(0, stringGiven.indexOf("hrs"));
         int hourToShow = Integer.parseInt(hourChaging);
-
-
+        
         String minuteChaging = stringGiven.substring(stringGiven.indexOf("and ") + 4, stringGiven.indexOf(" minutes"));
         int minuteToShow = Integer.parseInt(minuteChaging);
-
-
         int totalMinutes = hourToShow * 60 + minuteToShow;
-
+        
         System.out.println("Total minutes is " + totalMinutes);
     }
 }
